b402b185c93f05b07e80346028960ec9640a0483
Jan Szymanski <janohan@gmail.com>
2023-02-21 14:52:03 +0000

    Initial commit for 2021 Day10.

--- 2021/Day9/Day9_day2.py
+++ 2021/Day9/Day9_day2.py
@@ -1,37 +1,6 @@
 import numpy as np
-from helper_function import helper_functions as hf
 
-with open("input_files/example_input.txt", "r") as f:
-#with open("input_files/input.txt", "r") as f:
+#with open("input_files/example_input.txt", "r") as f:
+with open("input_files/input.txt", "r") as f:
         heightmap = f.readlines()
 
-heightmap_array = []
-
-for line in heightmap:
-    heightmap_array.append([int(a) for a in line.strip()])
-heightmap_array = np.array(heightmap_array)
-
-shape = heightmap_array.shape
-result = 0
-lowest_points = {}
-
-for x, row in enumerate(heightmap_array):
-    for y, var in enumerate(row):
-        ifis = hf.check_if_lowest(heightmap_array, x, y)
-        if ifis:
-            lowest_points[(x, y)] = var
-            result += var + 1
-
-print("The lowest points are: ", lowest_points)
-print("Result is: ", result)
-
-# starting part 2
-visited_basins = []
-
-
-def traverse_basin(heightmap_array, lowest_point):
-    pass
-
-
-for i, lowest_point in enumerate(lowest_points):
-    score = traverse_basin(heightmap_array, lowest_point)
--- 2021/Day9/helper_function.py
+++ 2021/Day9/helper_function.py
@@ -22,3 +22,28 @@ class helper_functions:
                 lowest = False
                 break
         return lowest
+
+    @staticmethod
+    def traverse_deeper(height_map, point: tuple[int, int], visited: [tuple[int, int]]) -> [tuple[int, int]]:
+        shape = height_map.shape
+        row, col = point
+        moves = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        b = []
+
+        for move in moves:
+            nrow = row + move[0]
+            ncol = col + move[1]
+
+            if nrow < 0 or nrow >= shape[0]:
+                continue
+            elif ncol < 0 or ncol >= shape[1]:
+                continue
+            elif height_map[nrow, ncol] == 9:
+                continue
+            elif (nrow, ncol) in visited:
+                continue
+            else:
+                b.append((nrow, ncol))
+                visited.append((nrow, ncol))
+
+        return b
