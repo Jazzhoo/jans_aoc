28a65383d946ba03f4b7f28415816c7a8cf72c00
Jan Szymanski <janohan@gmail.com>
2023-02-24 22:07:48 +0000

    Initial day 11 of 2021 commit.

--- 2021/Day10/Day10.py
+++ 2021/Day10/Day10.py
@@ -14,18 +14,18 @@ CLOSING_BR = score_match.keys()
 
 
 for i, line in enumerate(nav_system):
-    line_check = []
-    for char in line:
-        if char in OPENING_BR:
-            line_check.append(char)
-        elif char in CLOSING_BR:
-            if score_match[char][0] == line_check[-1]:
-                line_check.pop()
-                continue
-            else:
-                score_total += score_match[char][1]
-                discard_indexes.append(i)
-                break
+        line_check = []
+        for char in line:
+                if char in OPENING_BR:
+                        line_check.append(char)
+                elif char in CLOSING_BR:
+                        if score_match[char][0] == line_check[-1]:
+                                line_check.pop()
+                                continue
+                        else:
+                                score_total += score_match[char][1]
+                                discard_indexes.append(i)
+                                break
 
 print(f'Total score is: {score_total}')
 
@@ -34,7 +34,7 @@ print(f'Total score is: {score_total}')
 discard_indexes.sort(reverse=True)
 
 for idx in discard_indexes:
-    nav_system.pop(idx)
+        nav_system.pop(idx)
 
 line_closers = {'(': (')', 1), '[': (']', 2), '{': ('}', 3), '<': ('>', 4)}
 #score_total = 0
@@ -42,23 +42,23 @@ score_list = []
 pass
 
 for i, line in enumerate(nav_system):
-    line_check = []
-    for char in line:
-        if char in OPENING_BR:
-            line_check.append(char)
-        elif char in CLOSING_BR:
-            if score_match[char][0] == line_check[-1]:
-                line_check.pop()
-                continue
+        line_check = []
+        for char in line:
+                if char in OPENING_BR:
+                        line_check.append(char)
+                elif char in CLOSING_BR:
+                        if score_match[char][0] == line_check[-1]:
+                                line_check.pop()
+                                continue
 
-    score_total = 0
-    while line_check:
-        char = line_check.pop()
-        line.append(line_closers[char][0])
-        score_total *= 5
-        score_total += line_closers[char][1]
+        score_total = 0
+        while line_check:
+                char = line_check.pop()
+                line.append(line_closers[char][0])
+                score_total *= 5
+                score_total += line_closers[char][1]
 
-    score_list.append(score_total)
+        score_list.append(score_total)
 
 score_list.sort()
 middle_idx = (len(score_list) - 1) / 2
