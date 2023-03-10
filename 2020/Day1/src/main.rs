33f324fdcf8935b20a641eb128e2c223cc78ebac
Jan Szymanski <janohan@gmail.com>
2023-03-10 17:39:48 +0000

    Part 1 of Day 1 of 2020 in rust completed

--- 2020/Day1/src/main.rs
+++ 2020/Day1/src/main.rs
@@ -1,7 +1,31 @@
-use std::fs;
-
+use std::{fs, io::Read};
 fn main() {
-    let content = fs::read_to_string("src/example_input")
+    let mut file = fs::File::open("src/input")
         .expect("file not found");
-    println!("The input is: \n{content}");
+    let mut data = String::new();
+
+    file.read_to_string(&mut data)
+        .expect("error while reading file");
+
+    let data: Vec<&str> = data.split("\n").collect();
+    let data: Vec<u32> = data.iter().map(|x| x.parse().unwrap()).collect();
+    let result: u32;
+    //println!("{:?}", data);
+
+    'outer_loop: for x in &data {
+        for y in &data {
+            //println!("x: {x}, y: {y}");
+            if x == y {
+                continue;
+            }
+            else if x + y == 2020 {
+                result = y * x;
+                println!("Part1: the numbers are {x} {y}, and");
+                println!("Part1: the solution is {result}");
+                break 'outer_loop;
+            }
+        }
+    }
+
+
 }
