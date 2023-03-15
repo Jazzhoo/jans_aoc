ff599b24101d9d7eb6346bdcae4a570029fb64d5
Jan Szymanski <janohan@gmail.com>
2023-03-15 22:25:15 +0000

    In the middle of day_3 part 1 of 2020.

--- 2020/day_3/src/main.rs
+++ 2020/day_3/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, io::Read};
 fn main() {
-    let mut file = fs::File::open("src/input")
+    let mut file = fs::File::open("src/example_input")
         .expect("file not found");
     let mut data = String::new();
 
@@ -8,6 +8,26 @@ fn main() {
         .expect("error while reading file");
 
     let data: Vec<&str> = data.trim().split("\n").collect();
-    let mut pass_list: Vec<Password> = Vec::new();
-    println!("Hello, world!");
+    let mut tree_map: Vec<Vec<char>> = Vec::new();
+    let mut tree_map_bin: Vec<Vec<bool>> = Vec::new();
+
+    for (idx, row) in data.iter().enumerate() {
+        let row_conv: Vec<char> = row.chars().collect();
+        tree_map_bin.push(Vec::new());
+        for sqr in &row_conv {
+            let mut sqr_bin = false;
+            if *sqr == '#' {
+                sqr_bin = true;
+            }
+            tree_map_bin[idx].push(sqr_bin);
+        }
+        tree_map.push(row_conv);
+    }
+    println!("{:?}", tree_map_bin);
+    const MOVE = (3, 1);
+    for (idx, row) in tree_map_bin.iter().enumerate() {
+        
+    }
+
+
 }
