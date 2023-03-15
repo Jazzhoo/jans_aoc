3607dc8dc5bc56fd3b4a4655ef94e2b85028e5d5
Jan Szymanski <janohan@gmail.com>
2023-03-15 11:51:12 +0000

    Finishing the part 1 of day 2, 2020.

--- 2020/day_2/src/main.rs
+++ 2020/day_2/src/main.rs
@@ -8,7 +8,7 @@ struct Password {
 }
 
 fn main() {
-    let mut file = fs::File::open("src/example_input")
+    let mut file = fs::File::open("src/input")
         .expect("file not found");
     let mut data = String::new();
 
@@ -16,26 +16,33 @@ fn main() {
         .expect("error while reading file");
 
     let data: Vec<&str> = data.trim().split("\n").collect();
-    //let data: Vec<Vec<&str>> = data.split(" ").collect().collect();
     let mut pass_list: Vec<Password> = Vec::new();
 
-    println!("{:?}", data);
+    //println!("{:?}", data);
 
     for line in data {
-        //let temp_str: Vec<&str> = line.split(" ").collect();
         let temp_str: Vec<&str> = line.split_whitespace().collect();
-        let temp_instruction: Vec<char> = temp_str[0].chars().collect();
-        let (x, y) = (temp_instruction[0].to_digit(10).unwrap(),
-                        temp_instruction[2].to_digit(10).unwrap());
+        let temp_instruction: Vec<&str> = temp_str[0].split('-').collect();
+        let (x, y) = (temp_instruction[0].parse::<u32>().unwrap(),
+                        temp_instruction[1].parse::<u32>().unwrap());
         let temp_letter: char = temp_str[1].chars().next().unwrap();
         let temp_pass = temp_str[2];
-        println!("tup: {:?}, temp_letter: {}, temp_pass: {}",
-                 (x,y), temp_letter, temp_pass);
+        //println!("tup: {:?}, temp_letter: {}, temp_pass: {}",
+        //         (x,y), temp_letter, temp_pass);
         pass_list.push(Password {
             instruction: (x, y),
             letter: temp_letter,
             pass: String::from(temp_pass),
         });
     }
-        println!("{:?}", pass_list);
+
+    let mut result: u32 = 0;
+    for item in &pass_list {
+        let temp_count: u32 = item.pass.matches(item.letter).count() as u32;
+        if temp_count >= item.instruction.0 && temp_count <= item.instruction.1 {
+            result += 1;
+        }
+    }
+        //println!("{:?}", pass_list);
+        println!("The # of valid passwords is: {}", result);
 }
