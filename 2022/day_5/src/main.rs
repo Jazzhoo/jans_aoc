52a0e3d7e50f9f80fa5e1dc53c685cd61618f83d
Jan Szymanski <janohan@gmail.com>
2023-03-27 21:33:49 +0100

    Finishing part2 of day_5 of 2022 in rust! Yay!

--- 2022/day_5/src/main.rs
+++ 2022/day_5/src/main.rs
@@ -8,7 +8,7 @@ struct Instruction {
 }
 
 fn main() {
-    let input_raw = fs::read_to_string("src/inputs/example_input.txt");
+    let input_raw = fs::read_to_string("src/inputs/input.txt");
     let input_raw = match input_raw {
         Ok(file) => file,
         Err(err) => panic!("error occured {:?}", err),
@@ -18,14 +18,12 @@ fn main() {
     let mut instruction_raw: Vec<Instruction> = Vec::new();
     let mut crate_map: HashMap<u32, Vec<char>> = HashMap::new();
 
-
     for line in input {
         if line.contains('[') {
             crate_map_raw.push(line.chars().collect());
         }
         else if line.contains("move") {
             let temp_line: Vec<&str> = line.split_whitespace().collect();
-            //println!("temp_line: {:?}", temp_line);
             instruction_raw.push(Instruction { 
                 moves: temp_line[1].parse().unwrap(), 
                 from: temp_line[3].parse().unwrap(), 
@@ -46,17 +44,36 @@ fn main() {
     for key in &keys {
         crate_map.get_mut(&key).unwrap().reverse();
     }
-    println!("before action {:?}", crate_map);
-    for inst in instruction_raw {
+    let mut crate_map_2 = crate_map.clone();
+    //println!("before action {:?}", crate_map);
+    for inst in &instruction_raw {
         for _i in 0..inst.moves {
             let temp_chr = crate_map.get_mut(&inst.from).unwrap().pop().unwrap();
             crate_map.get_mut(&inst.to).unwrap().push(temp_chr);
         }
     }
-    println!("after action {:?}", crate_map);
+    //println!("after action {:?}", crate_map);
     let mut result = String::new();
     for i in 1..=*keys.iter().max().unwrap() {
         result.push(crate_map.get(&i).unwrap().last().unwrap().clone());
     }
+
+    println!("====Part 1 ====");
+    println!("The result is: {}", &result);
+
+    for inst in &instruction_raw {
+        let mut temp_chr: Vec<char> = Vec::new();
+        for _i in 0..inst.moves {
+            temp_chr.push(crate_map_2.get_mut(&inst.from).unwrap().pop().unwrap());
+        }
+        temp_chr.reverse();
+        crate_map_2.get_mut(&inst.to).unwrap().append(&mut temp_chr);
+    }
+    //println!("after action {:?}", crate_map);
+    let mut result = String::new();
+    for i in 1..=*keys.iter().max().unwrap() {
+        result.push(crate_map_2.get(&i).unwrap().last().unwrap().clone());
+    }
+    println!("====Part 2 ====");
     println!("The result is: {}", &result);
 }
