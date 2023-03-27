5a00b4bc182ce4df5554d3074a8339c50b5f4773
Jan Szymanski <janohan@gmail.com>
2023-03-27 22:31:33 +0100

    in the middle of part 2, day_4 of 2020.

--- 2020/day_4/src/main.rs
+++ 2020/day_4/src/main.rs
@@ -1,5 +1,4 @@
-use std::{fs, io::Read};
-use std::collections::HashMap;
+use std::fs;
 
 fn check_for_cid(input: &str) -> usize {
     if input.contains("cid") {
@@ -9,27 +8,50 @@ fn check_for_cid(input: &str) -> usize {
         return 1;
     }
 }
+fn validate_passport(input: &Vec<&str>) -> u32 {
 
+    println!("{:?}", input);
+    1
+}
 fn main() {
     //reading the datafile
-    let raw_data = fs::read_to_string("src/input").expect("error while reading file");
-    let data = raw_data.split("\n\n").map(|s| s.replace("\n", " "));
+    let raw_data = fs::read_to_string("src/example_input").expect("error while reading file");
+    let data = raw_data.split("\n\n").map(|s| s.replace("\n", " ")).collect::<Vec<String>>();
     //println!("{}", data.next().unwrap());
     let mut valid_count = 0;
-    for line in data {
+    for line in &data {
         let temp_vec: Vec<&str> = line.trim().split(" ").collect();
         let temp_vec_len = temp_vec.len();
-        println!("{:?} || #: {}, val: {}", temp_vec, temp_vec_len, check_for_cid(&line));
+        //println!("{:?} || #: {}, val: {}", temp_vec, temp_vec_len, check_for_cid(&line));
         match temp_vec_len {
             8 => valid_count += 1,
             7 => valid_count += check_for_cid(&line),
             _ => continue,
         };
     }
+    println!("==== Part 1 ====");
     println!("The # of valid passports is: {}", valid_count);
     //preparing the HashTable
-    
-    
 
+    println!("==== Part 2 debug ====");
+    let mut valid_count = 0;
+    
+    for line in &data {
+        let temp_vec: Vec<&str> = line.trim().split(" ").collect();
+        let temp_vec_len = temp_vec.len();
+        //println!("{:?} || #: {}, val: {}", temp_vec, temp_vec_len, check_for_cid(&line));
+        match temp_vec_len {
+            8 => valid_count += 1,
+            7 => valid_count += match check_for_cid(&line) {
+                0 => 0,
+                1 => validate_passport(&temp_vec),
+                _ => continue,
+            },
+            _ => continue,
+        };
+    }
+
+    println!("==== Part 2 ====");
+    println!("The # of valid passports is: {}", valid_count);
 
 }
