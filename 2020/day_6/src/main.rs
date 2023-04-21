0ce1b6d9e8f614c4afd9526d3fe01b8e13754251
Jan Szymanski <janohan@gmail.com>
2023-04-21 06:29:53 +0100

    Finishing part 2 of day 6 in RUST! Yay!

--- 2020/day_6/src/main.rs
+++ 2020/day_6/src/main.rs
@@ -1,22 +1,46 @@
 use std::fs;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap} ;
 
 fn main() {
     //reading the datafile
     let raw_data = fs::read_to_string("src/input").expect("error while reading file");
-    let data = raw_data.trim().split("\n\n").map(|s| s.replace("\n", "")).collect::<Vec<String>>();
+    let data = raw_data.trim().split("\n\n").collect::<Vec<&str>>();
+    let mut data_clear = Vec::new();
+    for group in &data{
+        data_clear.push(group.trim().split("\n").collect::<Vec<&str>>());
+    }
+    //println!("{:?}", data_clear);
+    //let data = raw_data.trim().split("\n\n").map(|s| s.replace("\n", "")).collect::<Vec<String>>();
     let mut result = 0;
-    println!("{:?}", data);
+    let mut result_2 = 0;
+    let mut ans_map = Vec::new();
 
-    for group in data {
+// ===== Part 1==========
+    for group in data_clear {
         let mut temp_set = HashSet::new();
-        for chr in group.chars() {
-            temp_set.insert(chr);
+        let mut temp_map = HashMap::new();
+
+        for person in &group {
+            for ans in person.chars() {
+                temp_set.insert(ans);
+                let count = temp_map.entry(ans).or_insert(0);
+                *count += 1;
+            }
+
         }
-        println!("the qunique set of {:?}", group);
-        println!("is {:?}", temp_set);
+        ans_map.push(temp_map.clone());
+        //println!("the qunique set of {:?}", group);
+        //println!("is {:?}", temp_set);
         result += temp_set.len();
+        for (_,val) in temp_map.into_iter() {
+            result_2 += val / group.len();
+        }
+    println!("for group {:?}, len: {}, result_2: {}", group, group.len(), result_2);
     }
     println!("The part 1 solution is: {}", result);
+
+    println!("The part 1 solution is: {}", result_2);
+
+    
 
 }
