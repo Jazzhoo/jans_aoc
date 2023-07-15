64491dd97fa2d7a605566749acf6863b1af079e1
Jan Szymanski <janohan@gmail.com>
2023-07-15 10:54:09 +0100

    Finishing part 1 of day11 2020 in Go!

--- 2020/day11_go/day11.go
+++ 2020/day11_go/day11.go
@@ -17,14 +17,14 @@ func duplicate_matrix(input_matrix [][]byte) [][]byte {
 }
 func look_around(input_matrix [][]byte, x int, y int, max_x int, max_y int) int {
 	var count int
-	for i := range []int{-1, 0, 1} {
+	for _, i := range []int{-1, 0, 1} {
 		new_y := y + i
 		if new_y < 0 || new_y >= max_y {
 			continue
 		}
-		for j := range []int{-1, 0, 1} {
+		for _, j := range []int{-1, 0, 1} {
 			new_x := x + j
-			if new_x == 0 && new_y == 0 {
+			if new_x == x && new_y == y {
 				continue
 			}
 			if new_x < 0 || new_x >= max_x {
@@ -33,7 +33,6 @@ func look_around(input_matrix [][]byte, x int, y int, max_x int, max_y int) int 
 			if input_matrix[new_y][new_x] == '#' {
 				count++
 			}
-
 		}
 	}
 
@@ -43,24 +42,61 @@ func look_around(input_matrix [][]byte, x int, y int, max_x int, max_y int) int 
 func main() {
 	fmt.Println("Hello from day 11 of 2020 in GO!")
 
-	puzzle := "./input_files/example_puzzle.txt"
+	// puzzle := "./input_files/example_puzzle.txt"
+	puzzle := "./input_files/main_puzzle.txt"
+
 	c, err := os.ReadFile(puzzle)
 	if err != nil {
 		log.Fatalln("Error reading file: ", err.Error())
 	}
 
-	content := bytes.Split(c, []byte("\n"))
-	content[0][0] = 'x'
-	content2 := duplicate_matrix(content)
-	content2[0][1] = 'y'
+	current_matrix := bytes.Split(c, []byte("\n"))
+	max_row := len(current_matrix)
+	max_col := len(current_matrix[0])
+	next_matrix := duplicate_matrix(current_matrix)
 
-	fmt.Println("Testing with bytes")
-	for _, line := range content {
-		fmt.Printf("%s\n", string(line))
-	}
+	// fmt.Println("Current matrix")
+	// for _, line := range current_matrix {
+	// 	fmt.Printf("%s\n", string(line))
+	// }
 
-	fmt.Println("Testing copy of slice of bytes")
-	for _, line := range content2 {
-		fmt.Printf("%s\n", string(line))
+	var changes int
+	for {
+		changes = 0
+		for row, _ := range current_matrix {
+			for col, _ := range current_matrix[0] {
+				if current_matrix[row][col] == 'L' {
+					seats_taken := look_around(current_matrix, col, row, max_col, max_row)
+					if seats_taken == 0 {
+						next_matrix[row][col] = '#'
+						changes++
+					}
+				} else if current_matrix[row][col] == '#' {
+					seats_taken := look_around(current_matrix, col, row, max_col, max_row)
+					if seats_taken >= 4 {
+						next_matrix[row][col] = 'L'
+						changes++
+					}
+				}
+			}
+		}
+		if changes == 0 {
+			break
+		}
+		// fmt.Println("\nNext matrix")
+		// for _, line := range next_matrix {
+		// 	fmt.Printf("%s\n", string(line))
+		// }
+		current_matrix = duplicate_matrix(next_matrix)
 	}
+	var result1 int
+	for row, _ := range next_matrix {
+		for col, _ := range next_matrix[0] {
+			if next_matrix[row][col] == '#' {
+				result1++
+			}
+		}
+	}
+	fmt.Printf("\nPart 1 result is: %d.\n ", result1)
+
 }
